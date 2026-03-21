8ebb64a9e1d4d81cfee71a89161b9daf9acb0a3c
luisafferreira-spec <luisa.f.ferreira@hotmail.com>
2026-03-21 11:00:13 -0700

    Add files via upload

---
+++ Blazerun/service-worker.js
@@ -0,0 +1,12 @@
+const CACHE = 'blazrun-v1';
+const FILES = ['/blazrun-v2.html', '/blazrun-karte.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
